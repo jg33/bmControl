03f04b2827338324d88d035c317607840426f1e2
Jesse Garrison <jesse@takethefort.com>
2016-08-08 15:52:49 -0700

    switching to CNMAT library

--- bmControl.ino
+++ bmControl.ino
@@ -1,9 +1,9 @@
 
 /*
  BM Control
- 
+
  This program allows for easy OSC and Serial control of Black Magic Cameras over SDI.
- 
+
  	Board Used:
    	* Arduino Ethernet
 
@@ -16,7 +16,7 @@
 
  OSC Message Formatting:
  /bmc/[camera number]/[command] [value(s)]
- 
+
 */
 
 // Libraries //
@@ -24,25 +24,33 @@
 #include <Wire.h>
 #include <BMDSDIControl.h>
 #include <Ethernet.h>
-#include <ArdOSC.h>
+#include <EthernetUdp.h>
+
+#include <OSCBundle.h>
+
+
 
 // Constants //
 #define DEFAULT_OSC_RECEIVE_PORT 8888
-#define DEFAULT_IP_ADDRESS 192,168,0,168
-#define STATUS_LED_PIN 13
+#define DEFAULT_IP_ADDRESS 192,168,0,66
+#define STATUS_LED_PIN 9
 
 // Ethernet Stuff //
-IPAddress targetIp(192,168,0,13);
-byte mac[] = {0x90, 0xA2, 0xDA, 0x10, 0x41, 0x80};
-IPAddress ip(DEFAULT_IP_ADDRESS);
+EthernetUDP Udp;
+IPAddress targetIp(192, 168, 0, 13);
+byte mac[] = {0x90, 0xA2, 0xDA, 0x10, 0x63, 0x08};
+byte ip[] = {DEFAULT_IP_ADDRESS};
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  //buffer to hold incoming packet,
 char  sendBuffer[] = "acknowledged...";       // a string to send back
 
 
 // OSC Stuff //
-OSCServer oscServer;
-unsigned int oscReceivePort = DEFAULT_OSC_RECEIVE_PORT;   
-float replyPort=8000; //where to respond to pings and gets
+//OSCServer oscServer;
+//OSCClient oscClient;
+int oscReceivePort = DEFAULT_OSC_RECEIVE_PORT;
+float replyPort = 8000; //where to respond to pings and gets
+OSCMessage toSend;
+
 
 // Camera Communication Stuff //
 const int                 shieldAddress = 0x6E;
@@ -52,107 +60,180 @@ BMD_SDICameraControl_I2C  sdiCam(shieldAddress);
 // Camera State //
 float currentAperture;
 float currentFocus;
+float currentExposure;
+int currentGain;
 
+
+//debug stuff
+int debugApt = 0; //testing aperture
+byte debugIp[]  = { 192, 168, 0, 13 };
 
 // ---- Main ---- //
-void setup(){
-	Serial.begin(11522);
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hi.");
+  
+  // Network Setup //
+  Ethernet.begin(mac, ip);
+  Udp.begin(oscReceivePort);
+  Serial.println("Enet Enabled.");
 
-	// Network Setup //
-	Ethernet.begin(mac,ip);
-	oscServer.begin(oscReceivePort);
+  // Camera Setup //
+  sdiCam.begin();
+  Wire.setClock(400000); // Set max I2C speed
+  sdiCam.setOverride(true);
+  Serial.println("SDI Enabled.");
 
-	// Camera Setup //
-	sdiCam.begin();
-  	Wire.setClock(400000); // Set max I2C speed
-  	sdiCam.setOverride(true);
-
-
-
-	// Top-Level Callbacks //
-	oscServer.addCallback("/ping", &ping);
-	oscServer.addCallback("/setReplyPort", &setReplyPort);
-
-	oscServer.addCallback("/bmc/^", &parseMsg);
-
-	oscServer.addCallback("/bmcRaw/void", &rawVoid);
-	oscServer.addCallback("/bmcRaw/fixed16", &rawFixed16);
+  // Top-Level Callbacks //
+//  oscServer.addCallback("/ping", &pingPong);
+//  oscServer.addCallback("/setReplyPort", &setReplyPort);
+//
+//  oscServer.addCallback("/bmc/^", &parseBmcMsg);
+//  oscServer.addCallback("/bmcRaw/void", &rawVoid);
+//  oscServer.addCallback("/bmcRaw/fixed16", &rawFixed16);
+//  Serial.println("OSC Set up.");
 
 
 }
 
-void loop(){
-	
+void loop() {
+ 
+  // check for new bundles ///
+   OSCBundle bndl;
+   int size;
+   
+   // --- receive a bundle  --- //
+   if( (size = Udp.parsePacket())>0) {
+     Serial.println("packet");
+
+
+    while(size--)
+        bndl.fill(Udp.read());
+        Serial.println("read");
+
+        if(!bndl.hasError()){
+           Serial.println("no err");
+             if(bndl.size() > 0) {
+                static int32_t sequencenumber=0;
+                bndl.route("/ping", pingPong);
+                Udp.beginPacket(Udp.remoteIP(), replyPort);
+                bndl.send(Udp);
+                Udp.endPacket();     
+
+             }
+        } else{
+         Serial.println("err."); 
+        }
+   }
+  // --- //
+  
+  
+  
+  Serial.print("Set Aperture to: ");
+  Serial.println(debugApt);
+  setAperture(0,debugApt);
+  debugApt++;
+  debugApt = debugApt % 16;
+  
+//  Udp.beginPacket(debugIp, outPort);
+//  toSend.beginMessage("/hi");
+//  toSend.addArgInt32(123);
+//  oscClient.send(&toSend);
+//  Serial.println("sent");
+//  toSend.flush();
+//  Serial.println("fushed");
+
+  delay(1000);
+  Serial.println("delayed");
+      
+
 }
 
-void parseMsg( OSCMessage *_msg){
-	int camera = 0; //get address 0
-	String cmd = "hello"; //get command
+void parseBmcMsg( OSCMessage &_msg) {
+  
+  int camera = 0; //get address 0
+//  String addr = _msg.getOSCAddress(); //get command
 
 
-	if(cmd=="hello"){
+  if (true) {
 
-		} else{
+  } else {
 
-			Serial.println("[ERR] command not regognized...");
-		}
+    Serial.println("[ERR] command not regognized...");
+  }
 
-	blink();
+  blink();
 
 }
 
 // ---- Misc ---- //
-void ping(OSCMessage *_msg){
+void pingPong(OSCMessage &_msg, int val) {
+  blink();
 
-	OSCMessage reply;
-	reply.setOSCAddress("/pong");
-	reply.setIpAddress(_msg.getIpAddress());
-	reply.setPortNumber(replyPort);
-	reply.addArdString("ok!");
-	blink();
+  Serial.println("pinged.");
+//  toSend.setAddress(debugIp,replyPort);
+//  toSend.beginMessage("/pong");
+//  toSend.addArgString("ok!");
+//  oscClient.send(&toSend);
+//  toSend.flush();
 
 }
 
-void setReplyPort(OSCMessage *_msg){
-	replyPort = _msg.getArgInt32(0);
+void setReplyPort(OSCMessage &_msg) {
+  //replyPort = _msg.getArgInt32(0);
 }
 
 
 // ---- Camera Commands ---- //
 
-void setAperture(int _camera, float _value){
-	// FORMAT: Float (-1 - 16.0)
-	currentAperture = _value;
-	sdiCam.writeCommandFixed16(_camera,0,2,0, currentAperture ); 
+void setAperture(int _camera, float _value) {
+  // FORMAT: Float (-1 - 16.0)
+  currentAperture = _value;
+  sdiCam.writeCommandFixed16(_camera, 0, 2, 0, currentAperture );
 
 }
 
-void setFocus(int _camera, float _value){
-	// FORMAT: Float (0.0=near, 1.0=far)
-	currentFocus = _value;
-	sdiCam.writeCommandFixed16(_camera,0,0,0, currentFocus ); //
+void setFocus(int _camera, float _value) {
+  // FORMAT: Float (0.0=near, 1.0=far)
+  currentFocus = _value;
+  sdiCam.writeCommandFixed16(_camera, 0, 0, 0, currentFocus ); //
 
 }
+
+void setExposure(int _camera, int _value) {
+  // FORMAT: Int32 (time in "us" (sp?))
+  currentExposure = _value;
+  sdiCam.writeCommandFixed16(_camera, 0, 0, 0, currentExposure); //
+
+}
+
+void setGain(int _camera, int _value) {
+  // FORMAT: Int32 (time in "us" (sp?))
+  currentGain = _value;
+  sdiCam.writeCommandFixed16(_camera, 0, 0, 0, currentGain); //
+
+}
+
 
 // ---- Raw Commands ---- //
-void rawVoid(OSCMessage *_msg){
-	int _cam = 1; //camera
-	int _cat = 0; //cateogry
-	int _param = 0; //parameter
+void rawVoid(OSCMessage &_msg) {
+  int _cam = 1; //camera
+  int _cat = 0; //cateogry
+  int _param = 0; //parameter
 
-	sdiCam.writeCommandVoid(_cam, _cat, _param);
+  sdiCam.writeCommandVoid(_cam, _cat, _param);
 
 }
 
-void rawFixed16(OSCMessage *_msg){
+void rawFixed16(OSCMessage *_msg) {
 
 }
 
 // ---- Utility Functions ---- //
-void blink(){
-	digitalWrite(STATUS_LED_PIN, HIGH);
-  	delay(50);
-  	digitalWrite(STATUS_LED_PIN, LOW);
-  	delay(50);
+void blink() {
+  digitalWrite(STATUS_LED_PIN, HIGH);
+  delay(50);
+  digitalWrite(STATUS_LED_PIN, LOW);
+  delay(50);
 }
 
---
+++ oscTester.maxpat
@@ -0,0 +1,148 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 7,
+			"minor" : 2,
+			"revision" : 3,
+			"architecture" : "x86",
+			"modernui" : 1
+		}
+,
+		"rect" : [ 423.0, 79.0, 640.0, 479.0 ],
+		"bglocked" : 0,
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 1,
+		"gridsize" : [ 15.0, 15.0 ],
+		"gridsnaponopen" : 1,
+		"objectsnaponopen" : 1,
+		"statusbarvisible" : 2,
+		"toolbarvisible" : 1,
+		"lefttoolbarpinned" : 0,
+		"toptoolbarpinned" : 0,
+		"righttoolbarpinned" : 0,
+		"bottomtoolbarpinned" : 0,
+		"toolbars_unpinned_last_save" : 0,
+		"tallnewobj" : 0,
+		"boxanimatetime" : 200,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"description" : "",
+		"digest" : "",
+		"tags" : "",
+		"style" : "",
+		"subpatcher_template" : "",
+		"boxes" : [ 			{
+				"box" : 				{
+					"id" : "obj-2",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 353.0, 131.0, 125.0, 20.0 ],
+					"style" : ""
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-7",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 357.0, 341.0, 34.0, 20.0 ],
+					"style" : "",
+					"text" : "print"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-6",
+					"linecount" : 3,
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 264.0, 341.0, 47.0, 44.0 ],
+					"style" : "",
+					"text" : "/analog/0 302"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-4",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 353.0, 281.0, 99.0, 20.0 ],
+					"style" : "",
+					"text" : "udpreceive 9999"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-3",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 184.0, 233.0, 69.0, 20.0 ],
+					"style" : "",
+					"text" : "\"/tone 787\""
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-1",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 87.0, 281.0, 205.0, 20.0 ],
+					"style" : "",
+					"text" : "udpsend 192.168.0.66 8888 CNMAT"
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-3", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-6", 1 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-4", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-7", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-4", 0 ]
+				}
+
+			}
+ ],
+		"dependency_cache" : [  ],
+		"autosave" : 0
+	}
+
+}
