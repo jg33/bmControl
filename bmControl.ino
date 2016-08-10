4f14a723bb3748f4349527e5c48a909a8028102e
Jesse Garrison <jesse@takethefort.com>
2016-08-10 10:53:20 -0400

    adding camera struct for multi cams

--- bmControl.ino
+++ bmControl.ino
@@ -12,7 +12,7 @@
 
  	3rd Party Libraries Used:
  	* Black Magic SDI Control Library
- 	* ArdOSC (**MARK KROPF VERSION: https://github.com/MarkKropf/ArdOSC)
+ 	* OSC (CNMAT)
 
  OSC Message Formatting:
  /bmc/[camera number]/[command] [value(s)]
@@ -25,15 +25,13 @@
 #include <BMDSDIControl.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-
 #include <OSCBundle.h>
-
-
 
 // Constants //
 #define DEFAULT_OSC_RECEIVE_PORT 8888
 #define DEFAULT_IP_ADDRESS 192,168,0,66
 #define STATUS_LED_PIN 9
+#define NUM_CAMERAS 2 //number of cameras to be controlled
 
 // Ethernet Stuff //
 EthernetUDP Udp;
@@ -43,29 +41,34 @@ byte ip[] = {DEFAULT_IP_ADDRESS};
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  //buffer to hold incoming packet,
 char  sendBuffer[] = "acknowledged...";       // a string to send back
 
-
 // OSC Stuff //
-//OSCServer oscServer;
-//OSCClient oscClient;
 int oscReceivePort = DEFAULT_OSC_RECEIVE_PORT;
 float replyPort = 8000; //where to respond to pings and gets
-OSCMessage toSend;
-
 
 // Camera Communication Stuff //
 const int                 shieldAddress = 0x6E;
 BMD_SDICameraControl_I2C  sdiCam(shieldAddress);
 
-
 // Camera State //
-float cam1_currentAperture;
-float cam1_currentFocus;
-float cam1_currentExposure;
-int cam1_currentGain;
+struct Camera{
+  int id;
+  float aperture;
+  float focus;
+  int exposure;
+  int sensorGain;
+  int whiteBalance;
+  float lift[4];
+  float gamma[4];
+  float gain[4];
+  
+  
+};
+Camera cameras[NUM_CAMERAS+1];
+
 
 //debug stuff
 int debugApt = 0; //testing aperture
-byte debugIp[]  = { 192, 168, 0, 13 };
+byte replyIp[]  = { 192, 168, 0, 13 };
 
 // ---- Main ---- //
 void setup() {
@@ -89,17 +92,23 @@ void setup() {
 // --- OSC Messages --- //
 void parseBmcMsg(OSCMessage &_msg, int offset) {
   
-  int camera = 0; //get address 0
-  //  String addr = _msg.getOSCAddress(); //get command
+  char address[256];
+  _msg.getAddress(address,0);
+  int cam = getAddressSegment(address,1).toInt(); //get address segment 1
+  String cmd = getAddressSegment(address,2); //get address segment 2
+  
+  Serial.println(cam);
+  Serial.println(cmd);
 
-
-  if (_msg.fullMatch("/bmc/1/aperture",0)) {
-    setAperture(1, _msg.getInt(0));
+  if (cmd == "aperture") {
+    setAperture(cam, _msg.getInt(0));
   } else if (_msg.fullMatch("/bmc/1/focus",0)) {
-    setFocus(1, _msg.getFloat(0));
+    setFocus(cam, _msg.getFloat(0));
   } else if (_msg.fullMatch("/bmc/1/exposure",0)) {
-    setExposure(1, _msg.getInt(0));
-  }else {
+    setExposure(cam, _msg.getInt(0));
+  } else if (_msg.fullMatch("/bmc/1/gain",0)) {
+    setGain(cam, _msg.getInt(0));
+  } else {
 
     Serial.println("[ERR] command not regognized...");
     char buff[16];
@@ -118,7 +127,7 @@ void pingPong(OSCMessage &_msg, int val) {
   Serial.println("pinged.");
   OSCMessage reply("/pong");
   reply.add((int32_t)1);
-  Udp.beginPacket(debugIp,replyPort);
+  Udp.beginPacket(replyIp,replyPort);
   reply.send(Udp);
   Udp.endPacket();
   reply.empty();
@@ -126,17 +135,25 @@ void pingPong(OSCMessage &_msg, int val) {
 
 }
 
-void setReplyPort(OSCMessage &_msg) {
-  //replyPort = _msg.getArgInt32(0);
+void setReplyIp(OSCMessage &_msg, int val){
+  replyIp[0] = _msg.getInt(0);
+  replyIp[1] = _msg.getInt(1);
+  replyIp[2] = _msg.getInt(2);
+  replyIp[3] = _msg.getInt(3);
+
 }
 
-void getStatus(OSCMessage &_msg, int camNum = 1){
+void setReplyPort(OSCMessage &_msg, int val) {
+  replyPort = _msg.getInt(0);
+}
+
+void getStatus(OSCMessage &_msg, int camNum){
   
   if(true){
     OSCMessage reply("/status");
     reply.add((int32_t)1);
-    reply.add("/status/1/exposure").add((int32_t)cam1_currentExposure);
-    Udp.beginPacket(debugIp,replyPort);
+    reply.add("/status/1/exposure").add((int32_t)cameras[1].exposure);
+    Udp.beginPacket(replyIp,replyPort);
     reply.send(Udp);
     Udp.endPacket();
     reply.empty();
@@ -155,23 +172,26 @@ void loop() {
    
    // --- receive a bundle  --- //
    if( (size = Udp.parsePacket())>0) {
-     Serial.println("packet");
+     Serial.println("Packet Received...");
 
 
     while(size--)
         bndl.fill(Udp.read());
-        Serial.println("read");
 
         if(!bndl.hasError()){
            Serial.println("no err");
              if(bndl.size() > 0) {
                 static int32_t sequencenumber=0;
                 bndl.route("/ping", pingPong);    
-                bndl.route("/bmc", parseBmcMsg);    
+                bndl.route("/bmc", parseBmcMsg);
+            
+                bndl.route("/bmcRawVoid", writeRawVoid );    
                 bndl.route("/getStatus", getStatus);
+                bndl.route("/setReplyPort", setReplyPort);
+
              }
         } else{
-         Serial.println("err."); 
+         Serial.println("Error."); 
         }
    }
   // --- //
@@ -195,35 +215,64 @@ void loop() {
 
 void setAperture(int _camera, float _value) {
   // FORMAT: Float (-1 - 16.0)
-  cam1_currentAperture = _value;
-  sdiCam.writeCommandFixed16(_camera, 0, 2, 0, cam1_currentAperture );
+  cameras[_camera].aperture = _value;
+  sdiCam.writeCommandFixed16(_camera, 0, 2, 0, cameras[_camera].aperture );
+
 
 }
 
 void setFocus(int _camera, float _value) {
   // FORMAT: Float (0.0=near, 1.0=far)
-  cam1_currentFocus = _value;
-  sdiCam.writeCommandFixed16(_camera, 0, 0, 0, cam1_currentFocus ); //
+  cameras[_camera].focus = _value;
+  sdiCam.writeCommandFixed16(_camera, 0, 0, 0, cameras[_camera].focus ); //
 
 }
 
 void setExposure(int _camera, int _value) {
   // FORMAT: Int32 (time in "us" (sp?))
-  cam1_currentExposure = _value;
-  sdiCam.writeCommandFixed16(_camera, 0, 0, 0, cam1_currentExposure); //
+  cameras[_camera].exposure = _value;
+  sdiCam.writeCommandInt32(_camera, 1, 5, 0, cameras[_camera].exposure); //
 
 }
 
 void setGain(int _camera, int _value) {
-  // FORMAT: 
-  cam1_currentGain = _value;
-  sdiCam.writeCommandFixed16(_camera, 0, 0, 0, cam1_currentGain); //
+  // FORMAT: Int8 (1x, 2x, 4x, 8x, 16x gain)
+  cameras[_camera].sensorGain = _value;
+  sdiCam.writeCommandInt8(_camera, 1, 1, 0, cameras[_camera].sensorGain); //
 
 }
 
+void setWhiteBalance(int _camera, int _value){
+  // FORMAT: Int16: White Balance in Kelvin (3200 - 7500)
+  cameras[_camera].whiteBalance = _value;
+  sdiCam.writeCommandInt8(_camera, 1, 2, 0, cameras[_camera].whiteBalance); //
+  
+}
+
+void setLift(int _camera, float _rgbl[4]){
+  // FORMAT: float [4] RGBL, -4.0 - 4.0
+  cameras[_camera].lift[0] = _rgbl[0];
+  cameras[_camera].lift[1] = _rgbl[1];
+  cameras[_camera].lift[2] = _rgbl[2];
+  cameras[_camera].lift[3] = _rgbl[3];
+
+  sdiCam.writeCommandFixed16(_camera, 8, 0, 0, cameras[_camera].lift ); //
+
+}
+
+void setGamma(int _camera, float _rgbl[4]){
+  // FORMAT: float [4] RGBL, -4.0 - 4.0
+  cameras[_camera].gamma[0] = _rgbl[0];
+  cameras[_camera].gamma[1] = _rgbl[1];
+  cameras[_camera].gamma[2] = _rgbl[2];
+  cameras[_camera].gamma[3] = _rgbl[3];
+
+  sdiCam.writeCommandFixed16(_camera, 8, 1, 0, cameras[_camera].gamma ); //
+
+}
 
 // ---- Raw Commands ---- //
-void rawVoid(OSCMessage &_msg) {
+void writeRawVoid(OSCMessage &_msg, int val) {
   int _cam = 1; //camera
   int _cat = 0; //cateogry
   int _param = 0; //parameter
@@ -242,5 +291,20 @@ void blink() {
   delay(50);
   digitalWrite(STATUS_LED_PIN, LOW);
   delay(50);
+}
+
+String getAddressSegment(String _fullAddress, int segment){ //for getting individual elements of OSC addresses
+  int startSlashIndex=0;
+  int endSlashIndex;
+  
+  for(int i=0;i<segment;i++){
+    startSlashIndex= _fullAddress.indexOf("/", startSlashIndex+1);
+  }
+  
+  endSlashIndex =  _fullAddress.indexOf("/", startSlashIndex+1);
+//  Serial.print("substring: ");
+//  Serial.println(_fullAddress.substring(startSlashIndex+1, endSlashIndex));
+  return _fullAddress.substring(startSlashIndex+1, endSlashIndex);
+  
 }
 
